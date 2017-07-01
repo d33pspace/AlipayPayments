fed1f43cc153522989774f7e6a7e050e19f41722
Jimmy McWhinney <jimmy.mcw@live.com>
2017-07-01 08:23:39 +0800

    Add migrations.

--- AlipayPayments/Data/Migrations/20170701002242_Initial.Designer.cs
+++ AlipayPayments/Data/Migrations/20170701002242_Initial.Designer.cs
@@ -8,9 +8,10 @@ using AlipayPayments.Data;
 namespace AlipayPayments.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170701002242_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
