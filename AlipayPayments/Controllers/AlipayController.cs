6e06dc1b357f60bc65ad02b06f2e69ce9afcdff8
meteoric9637 <meteoric_stone@hotmail.com>
2017-07-06 19:50:08 +0800

    complete alipay api integration

--- AlipayPayments/Controllers/AlipayController.cs
+++ AlipayPayments/Controllers/AlipayController.cs
@@ -39,20 +39,106 @@ namespace AlipayPayments.Controllers
         [HttpGet]
         public IActionResult Success(TradeModel trade)
         {
-            Console.WriteLine(trade);
+            SortedDictionary<string, string> sPara = GetRequestGet();
+
+            if (sPara.Count > 0)
+            {
+                Notify aliNotify = new Notify();
+                bool verifyResult = aliNotify.VerifyReturn(sPara, Request.Query["sign"]);
+
+                if (verifyResult)
+                {
+                    string out_trade_no = Request.Query["out_trade_no"];
+                    string trade_no = Request.Query["trade_no"];
+
+                    string trade_status = Request.Query["trade_status"];
+                    if (Request.Query["trade_status"] == "TRADE_FINISHED" || Request.Query["trade_status"] == "TRADE_SUCCESS")
+                    {
+                        
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+                else
+                {
+                    
+                }
+            }
+            else
+            {
+                
+            }
             return View();
         }
 
         [HttpGet]
-        public IActionResult Notify(TradeModel trade)
+        public async Task<IActionResult> Notify()
         {
-            Console.WriteLine(trade);
-            return RedirectToAction("Success");
+            SortedDictionary<string, string> sPara = GetRequestPost();
+            if (sPara.Count > 0)
+            {
+                Notify aliNotify = new Notify();
+                bool verifyResult = await aliNotify.Verify(sPara, Request.Form["notify_id"], Request.Form["sign"]);
+                if (verifyResult)
+                {
+                    string out_trade_no = Request.Form["out_trade_no"];
+                    string trade_no = Request.Form["trade_no"];
+                    string trade_status = Request.Form["trade_status"];
+
+                    if (Request.Form["trade_status"] == "TRADE_FINISHED")
+                    {
+                        
+                    }
+                    else if (Request.Form["trade_status"] == "TRADE_SUCCESS")
+                    {
+                        
+                    }
+                    else
+                    {
+                    }
+                }
+                else
+                {
+                    
+                }
+            }
+            else
+            {
+                
+            }
+            return View();
         }
 
         public IActionResult Error()
         {
             return View();
+        }
+
+        public SortedDictionary<string, string> GetRequestPost()
+        {
+            SortedDictionary<string, string> sArray = new SortedDictionary<string, string>();
+            Microsoft.AspNetCore.Http.IFormCollection coll;
+            coll = Request.Form;
+            ICollection<string> requestKeys = coll.Keys;
+            foreach (string key in requestKeys)
+            {
+                sArray.Add(key, Request.Form[key]);
+            }
+            return sArray;
+        }
+
+        public SortedDictionary<string, string> GetRequestGet()
+        {
+            SortedDictionary<string, string> sArray = new SortedDictionary<string, string>();
+            Microsoft.AspNetCore.Http.IQueryCollection coll = Request.Query;
+            ICollection<string> keys = coll.Keys;
+            foreach (string key in keys)
+            {
+                sArray.Add(key, Request.Query[key]);
+            }
+            return sArray;
         }
     }
 }
