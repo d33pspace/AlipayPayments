4fc64b4b496ae8486cbce4c627c6b15749d52edb
Jimmy McWhinney <jimmy.mcw@live.com>
2017-07-06 10:05:57 +0800

    Reenable SSL.

--- AlipayPayments/Startup.cs
+++ AlipayPayments/Startup.cs
@@ -49,10 +49,10 @@ namespace AlipayPayments
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            services.AddMvc(/*options =>
+            services.AddMvc(options =>
             {
                 options.Filters.Add(new RequireHttpsAttribute());
-            }*/);
+            });
 
             // Add application services.
             services.AddTransient<IEmailSender, AuthMessageSender>();
@@ -65,8 +65,8 @@ namespace AlipayPayments
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
-            /*var options = new RewriteOptions()
-                .AddRedirectToHttps();*/
+            var options = new RewriteOptions()
+                .AddRedirectToHttps();
 
             if (env.IsDevelopment())
             {
